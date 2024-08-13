7d12ab63a812bd39b5e84b27e10e37f37196d12d
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-08-13 22:38:58 +0200

    add current conditions basic attributes

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,17 @@ use serde::{Deserialize, Serialize};
 const URL: &str = "https://api.open-meteo.com/v1/forecast?latitude=46.2022&longitude=6.1457&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&models=icon_seamless";
 
 #[derive(Serialize, Deserialize, Debug)]
+struct Current {
+    time: String,
+    #[serde(rename = "temperature_2m")]
+    temperature: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Conditions {
     latitude: f32,
     longitude: f32,
+    current: Current,
 }
 
 #[tokio::main]
