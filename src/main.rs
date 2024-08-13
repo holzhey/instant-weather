5b3de96a660cd1c926399756253fcb6e3c940c42
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-08-13 22:33:41 +0200

    add initial conditions structure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use serde::{Deserialize, Serialize};
+
 const URL: &str = "https://api.open-meteo.com/v1/forecast?latitude=46.2022&longitude=6.1457&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&models=icon_seamless";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Conditions {
+    latitude: f32,
+    longitude: f32,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get(URL).await?.json::<serde_json::Value>().await?;
+    let resp = reqwest::get(URL).await?.json::<Conditions>().await?;
     println!("{resp:#?}");
     Ok(())
 }
