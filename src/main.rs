f4a020d680ed313d515864bbcc44011c911a9fda
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-08-13 22:29:52 +0200

    add static weather request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+const URL: &str = "https://api.open-meteo.com/v1/forecast?latitude=46.2022&longitude=6.1457&current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,rain,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&models=icon_seamless";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get(URL).await?.json::<serde_json::Value>().await?;
+    println!("{resp:#?}");
+    Ok(())
 }
