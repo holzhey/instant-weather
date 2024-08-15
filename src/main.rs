9543644323cd1cfc00b681c68060eba3acad86aa
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2024-08-15 21:21:54 +0200

    combine multiple assignments into one

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,17 @@ const URL: &str = "https://api.open-meteo.com/v1/forecast?latitude=46.2022&longi
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get(URL).await?.json::<serde_json::Value>().await?;
-    if let Some(values) = resp.get("current").unwrap().as_object() {
-        if let Some(units) = resp.get("current_units").unwrap().as_object() {
-            for v in values {
-                println!("{}: {} {}", v.0, v.1, units[v.0].as_str().unwrap());
-            }
+    if let (Some(values), Some(units)) = (
+        resp.get("current").unwrap().as_object(),
+        resp.get("current_units").unwrap().as_object(),
+    ) {
+        for value in values {
+            println!(
+                "{}: {}{}",
+                value.0,
+                value.1,
+                units[value.0].as_str().unwrap()
+            );
         }
     }
     Ok(())
